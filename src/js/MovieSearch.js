6aec7df4207171f96b63a6c40aa9e6eda032b2ca
James Adams <inklink28@gmail.com>
2022-03-17 13:34:47 -0400

    Initial Commit of hello world app.

---
+++ src/js/MovieSearch.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../css/MovieSearch.css'
+
+function MovieSearch(props){
+  return (
+    <div className="movieSearch">
+      <div class="search page">
+        <header>
+          <div className="logo">Movie Search Challenge</div>
+        </header>
+
+        <div className="pageWrapper">
+          <div className="content">
+            Hello World
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MovieSearch;
